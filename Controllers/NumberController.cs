1be39fefde398b850e6e9b15a42585d0783b14db
NotWhoops <whoopsplayz@gmail.com>
2026-01-09 16:29:54 -0800

    first commit

---
+++ Controllers/NumberController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using VanhornBMC4.Services;
+
+
+namespace VanhornBMC4.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NumberController : ControllerBase
+    {
+         private readonly AddNumbers _addNumberServices;
+
+         public NumberController(AddNumbers addNumberServices)
+        {
+            _addNumberServices = addNumberServices;
+        }
+         
+        [HttpGet]
+        [Route("AddingNumbers/{num1}/{num2}")]
+        public string AddingNumbers(int num1, int num2)
+        {
+            return _addNumberServices.
+            AddingNumbers(num1, num2);   
+        }
+        [HttpGet]
+        [Route("BiggerNumbers/{num1}/{num2}")]
+        public string BiggerNumbers(int num1, int num2)
+        {
+            return _addNumberServices.
+            BiggerNumbers(num1, num2);   
+        }
+        [HttpGet]
+        [Route("SmallerNumbers/{num1}/{num2}")]
+        public string SmallerNumbers(int num1, int num2)
+        {
+            return _addNumberServices.
+            SmallerNumbers(num1, num2);   
+        }
+    }
+}
---
+++ Services/AddNumbersServices.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VanhornBMC4.Services
+{
+    public class AddNumbers
+    {
+        public string AddingNumbers(int num1, int num2)
+        {
+            return $"the sum of {num1} + {num2} = {num1 + num2}";
+        }
+        public string BiggerNumbers(int num1, int num2)
+        {
+            return $"{num1} > {num2} = {num1} > {num2}";
+        }
+        public string SmallerNumbers(int num1, int num2)
+        {
+            return $"{num1} < {num2} = {num1} < {num2}";
+        }
+    }
+}
