f31c719e4e10a1288431b609d5a09d8a74d918a5
NotWhoops <whoopsplayz@gmail.com>
2026-01-12 11:20:08 -0800

    stuff not sure

--- obj/Debug/net9.0/Todov4.AssemblyInfo.cs
+++ obj/Debug/net9.0/Todov4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Todov4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50e95d4b9068ae1305bef94e79210cb0bb394835")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43bb392291ec760637c8d918f2c1279624e24363")]
 [assembly: System.Reflection.AssemblyProductAttribute("Todov4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Todov4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
