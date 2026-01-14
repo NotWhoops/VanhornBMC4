60cbf408883a0e2cfdc040d4f65baaa3e9155032
NotWhoops <whoopsplayz@gmail.com>
2026-01-14 08:31:30 -0800

    fixed linking issues in services

--- Controllers/GreaterOrLessController.cs
+++ Controllers/GreaterOrLessController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using VanhornBMC4.Services;
 
 namespace VanhornBMC4.Controllers
 {
@@ -10,18 +11,25 @@ namespace VanhornBMC4.Controllers
     [Route("api/[controller]")]
     public class GreaterOrLessController : ControllerBase
     {
+        private readonly GreaterOrLessThanServices _greaterorlessThanServices;
+
+        public GreaterOrLessController(GreaterOrLessThanServices greaterorlessThanServices)
+        {
+            _greaterorlessThanServices = greaterorlessThanServices;
+        }
+
         [HttpGet]
         [Route("BiggerNumbers/{num1}/{num2}")]
         public string BiggerNumbers(int num1, int num2)
         {
-            return _GreaterOrLessThanServices.
+            return _greaterorlessThanServices.
             BiggerNumbers(num1, num2);   
         }
         [HttpGet]
         [Route("SmallerNumbers/{num1}/{num2}")]
         public string SmallerNumbers(int num1, int num2)
         {
-            return _GreaterOrLessThanServices.
+            return _greaterorlessThanServices.
             SmallerNumbers(num1, num2);   
         }
     }
